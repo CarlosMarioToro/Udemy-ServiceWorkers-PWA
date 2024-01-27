688feaf1b93485155f0363d5a6f6ad58b5117f4d
CarlosMarioToro <111100510+CarlosMarioToro@users.noreply.github.com>
2024-01-27 08:07:04 -0500

    351-Agregar soporte Offline

--- sw.js
+++ sw.js
@@ -1,11 +1,40 @@
+const nombreCache = 'apv-v1';
+const archivos = [
+    './',
+    './index.html',
+    './css/bootstrap.css',
+    './css/styles.css',
+    './js/app.js',
+    './js/apv.js',
+];
+
 // Cuando se instala el Service Worker
 self.addEventListener('install', e => {
     console.log('instalado el Service Worker');
-    console.log(e);
+
+    e.waitUntil(
+        caches.open(nombreCache)
+            .then(cache => {
+                console.log('Cacheando');
+                cache.addAll(archivos)
+            })
+    );
 });
 
 // Activar el Service Worker
 self.addEventListener('activate', e => {
     console.log("Activando el servicio worker");
     console.log(e);
+});
+
+// Evento fetch para descargar archivos estatico
+self.addEventListener('fetch', e => {
+    console.log('Fetch...', e);
+
+    e.respondWith(
+        caches.match(e.request)
+        .then(respuestaCache => {
+            return respuestaCache
+        })
+    )
 })
